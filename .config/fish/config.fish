42bd575f88148c406ee7ceb6561f2268f0f4e188
benf <benjamin.frazer.1997@gmail.com>
2024-06-17 15:56:18 +0100

    added fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+set -g fish_greeting
+if status is-interactive
+    fish_git_prompt
+    # set -g theme_display_ruby no        # Disables displaying the current ruby version
+    # set -g theme_display_virtualenv no  # Disables displaying the current virtualenv name
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_user_key_bindings 
+    fish_vi_key_bindings
+    bind --erase --preset \ek
+    bind --erase --preset \ej
+    bind --erase --preset \eh
+    bind --erase --preset \el
+end
+
+
+direnv hook fish | source
